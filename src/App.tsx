bc9010e342e6cd8df18e1bfcd6b395b29e00bba0
Shivam Patel <patelsd96@gmail.com>
2024-11-06 15:42:07 -0500

    Took out grid css

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,18 @@ function App() {
   const resetPoints = useScoreStore((state) => state.resetPoints);
   return (
     <>
-      <section className="grid grid-cols-6 grid-rows-7 gap-5">
-        <h1 className="text-3xl font-bold underline border-solid border-2 border-blue-500 col-start-2 col-span-4 row-start-1 row-span-1 justify-self: center">
+      <section className="flex">
+        <h1 className="text-3xl font-bold underline border-solid border-2 border-blue-500">
           Score Tracker
         </h1>
-        <div className="border-solid border-2 border-red-500 col-start-3 col-span-2 row-start-2 row-end-3 justify-self: center">
+        <div className="border-solid border-2 border-red-500">
           <button type="submit" onClick={() => resetPoints()}>
             {" "}
             Click Here to Reset
           </button>
         </div>
-        <div
-          id="cards"
-          className="col-start-1 col-end-7 row-start-3 row-end-8 justify-self: center"
-        >
-          <div className="border-solid border-2 border-sky-500  ">
+        <div id="cards" className="flex">
+          <div className="border-solid border-2 border-sky-500 ">
             <TeamACard />
           </div>
           <div className="border-solid border-2 ">
